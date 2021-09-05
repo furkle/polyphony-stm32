ac9dd58623eaa0174b0c5a60b00e9b21f8cc824a
furkle <furkle@furkleindustries.com>
2021-09-05 16:42:12 -0400

    Add files via upload

---
+++ Debug/Core/Src/buffer.d
@@ -0,0 +1,6 @@
+Core/Src/buffer.o: ../Core/Src/buffer.c ../Core/Inc/buffer.h \
+ ../Core/Inc/misc.h
+
+../Core/Inc/buffer.h:
+
+../Core/Inc/misc.h:
---
+++ Debug/Core/Src/midi.d
@@ -0,0 +1,8 @@
+Core/Src/midi.o: ../Core/Src/midi.c ../Core/Inc/buffer.h \
+ ../Core/Inc/midi.h ../Core/Inc/misc.h
+
+../Core/Inc/buffer.h:
+
+../Core/Inc/midi.h:
+
+../Core/Inc/misc.h:
